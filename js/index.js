447d6601ef76252f0b6744e450f6d0239da84088
VulpesDevo <vulpescodev@gmail.com>
2023-12-09 16:20:48 +0800

    fixed duplicate data added/ enable and disable form-control

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
-const tabs=document.querySelectorAll('.tab-btn' ) ;
-const all_content=document.querySelectorAll('._content') ;
-tabs. forEach((tab, index)=>{
-    tab.addEventListener('click',()=>{
-        tabs.forEach(tab=>{tab.classList.remove('active')});
-        tab.classList.add('active');
-        all_content.forEach(content=>{content.classList.remove('active')});
-        all_content[index].classList.add('active');
-    })
-})
+// const tabs=document.querySelectorAll('.tab-btn' ) ;
+// const all_content=document.querySelectorAll('._content') ;
+// tabs. forEach((tab, index)=>{
+//     tab.addEventListener('click',()=>{
+//         tabs.forEach(tab=>{tab.classList.remove('active')});
+//         tab.classList.add('active');
+//         all_content.forEach(content=>{content.classList.remove('active')});
+//         all_content[index].classList.add('active');
+//     })
+// })
 const tabSelector = document.getElementById('tabSelector');
 const allContent = document.querySelectorAll('._content');
 const tableRows = document.querySelectorAll('#table-onqueue tbody tr');
@@ -88,23 +88,38 @@ let assignedTeam;
 let stat;
 let priority;
 let deadline;
+let tagetDate;
 let modalHeader = document.querySelector('.modal-ako');
+let modalStat = document.querySelector('#field-status');
+let currentDate;
+currentDate = new Date();
+
+// Add 7 days to the current date
+let sevenDaysLater = new Date(currentDate);
+sevenDaysLater.setDate(currentDate.getDate() + 7);
+
+
+// change
 function changeModalHeaderColor(status){
     console.log(status);
     switch (status){
         case 'IN PROGRESS':
             modalHeader.classList.add('bg-warning');
+            modalStat.classList.add('bg-warning');
             break;     
         case 'SOLVED':
             modalHeader.classList.add('bg-success');
+            modalStat.classList.add('bg-success');
+            
             console.log(modalHeader);
             break;    
         default:
             modalHeader.classList.add('bg-secondary');
+            modalStat.classList.add('bg-secondary');
             break;                    
     }
 }
-function assignRowFieldValues(){
+function assignRowFieldValues(row){
     let title = document.getElementById('request-title');
     let _stat = document.getElementById('field-status');
     let tgt_dl = document.getElementById('deadline');
@@ -164,6 +179,11 @@ function showHideModalButtons(row, state =''){
     });
 
     if(stats.includes("IN PROGRESS")){
+        const inputFields = document.querySelectorAll(".form-control");
+            inputFields.forEach(input => {
+                if(input.id != "date-completed" && input.id != "field-status") input.removeAttribute("disabled");
+                console.log("form fields");
+            });
         if(state ==''){
             showBtns = modalMain.querySelector('#modal-btn-process');
             showBtns.classList.remove('d-none');
@@ -173,8 +193,29 @@ function showHideModalButtons(row, state =''){
         }
     }else if(stats.includes('SOLVED')){
         //maybe
+        modalStat.classList.remove('bg-warning');
+        modalStat.classList.remove('bg-secondary');
+        modalHeader.classList.add('bg-success');
+        modalStat.classList.add('bg-success');
+        const inputFields = document.querySelectorAll(".form-control");
+            inputFields.forEach(input => {
+                if(input.id != "date-completed" && input.id != "field-status") input.setAttribute("disabled","");
+                console.log("form fields");
+            });
+            
         console.log('in else if');
+    }else if(stats.includes('CANCELLED')){
+        modalStat.classList.remove('bg-warning');
+        modalStat.classList.remove('bg-success');
+        modalHeader.classList.add('bg-secondary');
+        modalStat.classList.add('bg-secondary');
+        const inputFields = document.querySelectorAll(".form-control");
+            inputFields.forEach(input => {
+                if(input.id != "date-completed" && input.id != "field-status") input.setAttribute("disabled","");
+                console.log("form fields");
+            });
     }
+    
 }
 function clearFieldValues(){
     let title = document.getElementById('request-title');
@@ -198,6 +239,8 @@ function clearFieldValues(){
     assign_team.value = '';
     _priority.value = '';
 }
+
+
 function addTicketRecord(){
     let title = document.getElementById('request-title');
     let tgt_dl = document.getElementById('deadline');
@@ -206,10 +249,14 @@ function addTicketRecord(){
     let _cust = document.getElementById('requested-by');
     let assign_team = document.getElementById('assigned-team');
     let _priority = document.getElementById('priority');
-    let prio= _priority.options[_priority.selectedIndex].value;
+    let curDate = document.getElementById('date-created');
+    curDate.value = currentDate;
+    tgt_dl.value= sevenDaysLater;
+    let  prio = _priority.options[_priority.selectedIndex].value;
+    // currentDate
     const tblRow   = document.querySelector("#table-onqueue");
     const tblBody  = tblRow.querySelector('tbody');
-    
+
     let newRow  = tblBody.insertRow();
     let col1 = newRow.insertCell(0); //ticket no
     let col2 = newRow.insertCell(1); //customer name
@@ -219,6 +266,7 @@ function addTicketRecord(){
     let col6 = newRow.insertCell(5); 
     let col7 = newRow.insertCell(6); //priority
     let col8= newRow.insertCell(7); //deadline
+    let col9= newRow.insertCell(8); //deadline
     col1.outerHTML = `<th class="align-middle fs-6">TIX-1000</th>`;
     col2.outerHTML = `<td class="align-middle fs-6">${_cust.value}</td>`;
     col3.outerHTML = `<td class="align-middle fs-6">${accNo.value}</td>`;
@@ -227,6 +275,9 @@ function addTicketRecord(){
     col6.outerHTML = `<td class="align-middle fs-6">IN PROGRESS</td></td>`;
     col7.outerHTML = `<td class="align-middle fs-6">${prio}</td>`;
     col8.outerHTML = `<td class="align-middle fs-6">${tgt_dl.value}</td>`;
+    col9.outerHTML = `<td class="align-middle fs-6">${curDate.value}</td>`;
+
+
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -251,17 +302,17 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log(solvedRowCount);
     rows.forEach(row => {
         const columns = row.getElementsByTagName('td');
-        if(columns[6].textContent == 'completed'){
+        if(stat == 'SOLVED'){
             let removeButtons = columns[7].querySelectorAll(".btn-warning,.btn-danger");
             removeButtons[0].classList.add('d-none'); //edit
             removeButtons[1].classList.add('d-none');
         }
     });
     let _rows = document.querySelectorAll('tbody tr');
-    _rows.forEach(function(row) {
+    _rows.forEach(function(row, index) {
         row.addEventListener('click', function() {
-
-            ticketNo = this.getElementsByTagName('th')[0].textContent;
+            let rows = row.parentElement.parentElement;
+            ticketNo = row.getElementsByTagName('th')[0].textContent;
             let columns = this.getElementsByTagName('td');
             activeRow = row;
             customer = columns[0].textContent;
@@ -271,25 +322,71 @@ document.addEventListener('DOMContentLoaded', function() {
             stat = columns[4].textContent;
             priority = columns[5].textContent;
             deadline = columns[6].textContent;
-            console.log(row.classList.add());
+            tagetDate = columns[7].textContent;
+
+            // Now you can use the 'index' variable to get the index of the clicked row
+            
+            
+                  // Get the data from the clicked row
+            
+            const firstName = columns[1].textContent;
+            const lastName = columns[2].textContent;
+    
+            const status = document.getElementById("field-status");
+            console.log(columns[0].textContent);
+            
+            const animalArray = [];
+            for (var i = 0; i < status.length; i++) {
+            animalArray.push(status[i].textContent);
+            }
+            
+        
         });
-    });
+        
+});
     let activeRow = null;
     viewButton = document.querySelectorAll('.viewer') ;
     viewButton.forEach(function(button){
         button.addEventListener('click', function(){
+            let title = document.getElementById('request-title');
+            let _stat = document.getElementById('field-status');
+            let tgt_dl = document.getElementById('deadline');
+            let modalTitle = document.getElementById('modals-ticket-no');
+            let _cust = document.getElementById('requested-by');
+            let assign_team = document.getElementById('assigned-team');
+            // let _priority = document.getElementById('priority');
+            let account = document.getElementById('account-no');
+            let targetdate = document.getElementById('date-created');
+            const priorityDropdown = document.getElementById('priority');
+            modalTitle.value = ticketNo;
+            title.value = issueDescription;
+            _stat.value = stat;
+            tgt_dl.value = deadline;
+            _cust.value = customer;
+            assign_team.value = assignedTeam;
+            account.value = accountNumber;
+            targetdate.value= tagetDate;
+            
+            switch(priority.toUpperCase()) {
+                case"HIGH":
+                    priorityDropdown.selectedIndex = 0;
+                    break;
+                case"MEDIUM":
+                    priorityDropdown.selectedIndex = 1;
+                    break;
+                default:
+                    priorityDropdown.selectedIndex = 2;
+                    break;
+            }
             let row = this.parentElement.parentElement;
          // assign values to fields base on selected row
-            assignRowFieldValues();
+            // assignRowFieldValues(row);
             showHideModalButtons(row,'edit');
-            console.log(this.parentElement.parentElement);
-            
             // remove disable attribute in fields
-            const inputFields = document.querySelectorAll(".form-control");
-            inputFields.forEach(input => {
-                if(input.id != "date-completed" && input.id != "field-status") input.removeAttribute("disabled");
-            });
-            
+            // const inputFields = document.querySelectorAll(".form-control");
+            // inputFields.forEach(input => {
+            //     if(input.id != "date-completed" && input.id != "field-status") input.removeAttribute("disabled");
+            // });
             modalHeader.classList.remove('bg-warning','bg-success','bg-danger');
             changeModalHeaderColor(stat);
         });
@@ -298,24 +395,6 @@ document.addEventListener('DOMContentLoaded', function() {
     mdlSaveButton.addEventListener('click', function(){
         const columns  = activeRow.querySelectorAll('td');
         const modalMain = document.querySelector('#viewTicketModal');
-        
-        // customer = columns[0].textContent;
-        // contactNumber = columns[1].textContent;
-        // issueDescription = columns[2].textContent;
-        // assignedTeam = columns[3].textContent;
-        // stat = columns[4].textContent;
-        // priority = columns[5].textContent;
-        // deadline = columns[6].textContent;
-
-        // let title = document.getElementById('request-title');
-        // let _stat = document.getElementById('field-status');
-        // let tgt_dl = document.getElementById('target-date');
-        // let modalTitle = document.getElementById('modals-ticket-no');
-        // let _cust = document.getElementById('requested-by');
-        // let assign_team = document.getElementById('assigned-team');
-        // let _priority = document.getElementById('priority');
-        // let row = this.parentElement.parentElement;
-        
 
         let _priority = document.getElementById('priority');
         let prio= _priority.options[_priority.selectedIndex].value;
@@ -327,19 +406,6 @@ document.addEventListener('DOMContentLoaded', function() {
         columns[5].textContent = prio;
         columns[6].textContent = modalMain.querySelector('#deadline').value;
         console.log(modalMain.querySelector('#request-title').value);
-
-        // if (activeRow) {
-        //     const columns = activeRow.querySelectorAll('td')[0];
-        //     const modalMain = document.querySelector('#viewTicketModal');
-        //     // Check if columns is not null or undefined
-        //     if (columns) {
-        //         columns.textContent = modalMain.querySelector('#request-title').value;
-        //     } else {
-        //         console.error('Columns not found in activeRow');
-        //     }
-        // } else {
-        //     console.error('Active row is not set');
-        //}
 
     });
     deleteButton = document.querySelectorAll('.delete-ticket');
@@ -359,19 +425,23 @@ document.addEventListener('DOMContentLoaded', function() {
     addButton = document.querySelector('#add-ticket');
     addButton.addEventListener('click', function(){
         console.log("ADD BUTTON WAS CLICK");
+        
         const inputFields = document.querySelectorAll(".form-control");
         inputFields.forEach(input => {
             if(input.id != "date-completed" && input.id != "field-status") input.removeAttribute("disabled");
+
         });
-        const createButton = document.querySelector("#modal-btn-create");
-        createButton.classList.remove("d-none");
-        createButton.addEventListener('click', function(){
-            console.log("create ticket button");
-            addTicketRecord()
-        });
+        
     
         clearFieldValues();
 
+    });
+    const createButton = document.querySelector("#modal-btn-create");
+    createButton.classList.remove("d-none");
+    createButton.addEventListener('click', function(){
+        console.log("create ticket button");
+        
+        addTicketRecord()
     });
 
 
